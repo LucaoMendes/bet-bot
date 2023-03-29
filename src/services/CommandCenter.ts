4f137d38504af41e8c94571979f4d2aee318c625
LucaoMendes <lucas1999mendes@gmail.com>
2023-03-29 17:59:39 -0300

    [CommandCenter] Ajuste para tratamento de exceções

--- src/services/CommandCenter.ts
+++ src/services/CommandCenter.ts
@@ -2,6 +2,7 @@ import { Context , Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters'
 import { Update } from 'typegram'
 import { iCommand } from "../interfaces/iCommand"
+import User from '../models/User'
 import Logger, { LogType } from "../utils/Logger"
 
 export class CommandCenter {
@@ -11,6 +12,27 @@ export class CommandCenter {
     public static init(){
         this.bot = new Telegraf(process.env.BOT_TOKEN as string)
 
+        this.bot.use(async (ctx, next) => {
+            if(!ctx.from) {
+                await ctx.reply("ERROR: Não foi possível obter o seus dados")
+                return
+            } 
+
+            console.time(`Tempo de resposta para [${ctx.from?.id}] ${ctx.from?.first_name}`)
+
+            const user = await User.findOne({where:{chat_id: ctx.from?.id}})
+
+            ctx.state.data = user?.dataValues
+
+            try{
+                await next()
+            }catch(e){
+                Logger.send(`Erro ${e}`, LogType.ERROR)
+            }
+
+            console.timeEnd(`Tempo de resposta para [${ctx.from?.id}] ${ctx.from?.first_name}`)
+        })
+
         this.commands.forEach(command => {
             if(command.command === 'start') this.bot.start(command.function)
             else if(command.command === 'help') this.bot.help(command.function)
@@ -18,8 +40,12 @@ export class CommandCenter {
             
         })
 
-        this.bot.on(message('text'),this.whatDidUSay)
+        this.bot.action('/help', ctx => {
+            return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`)
+        })
 
+        this.bot.on(message('text'),this.whatDidUSay)
+        
         this.bot.launch()
 
         process.once('SIGINT', () => this.bot.stop('SIGINT'))
