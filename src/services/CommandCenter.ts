b96b2306238271f67c54ec32cf05cffb59d93465
LucaoMendes <lucas1999mendes@gmail.com>
2023-03-28 22:12:54 -0300

    [CommandCenter] Adicionado o log e a mensagem quando alguma informação não é compreendida

--- src/services/CommandCenter.ts
+++ src/services/CommandCenter.ts
@@ -1,4 +1,5 @@
 import { Context , Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
 import { Update } from 'typegram'
 import { iCommand } from "../interfaces/iCommand"
 import Logger, { LogType } from "../utils/Logger"
@@ -14,23 +15,39 @@ export class CommandCenter {
             if(command.command === 'start') this.bot.start(command.function)
             else if(command.command === 'help') this.bot.help(command.function)
             else this.bot.command(command.command, command.function)
+            
         })
 
+        this.bot.on(message('text'),this.whatDidUSay)
+
         this.bot.launch()
+
+        process.once('SIGINT', () => this.bot.stop('SIGINT'))
+        process.once('SIGTERM', () => this.bot.stop('SIGTERM'))
     }
 
     public static registerCommand(command: iCommand) {
         Logger.send(`Registrando comando ${command.command}`, LogType.INFO)
+
         if(!command || !command.command || !command.description || !command.function)
             return Logger.send(`Comando inválido ${JSON.stringify(command)}`,LogType.ERROR)
+
         this.commands.push(command)
     }
 
     public static executeCommand(commandName: string, ctx: Context) {
+        Logger.send(`Executando comando ${commandName} para [${ctx.from?.id}] ${ctx.from?.first_name}`, LogType.INFO)
         const command:iCommand | undefined = this.commands.find(x => x.command === commandName)
-        if (command) {
-            command.function(ctx)
-        }
+
+        if (command && command.function)
+            return command.function(ctx)
+
+        Logger.send(`Comando ${commandName} não encontrado`, LogType.ERROR)
+        throw new Error(`COMMAND CENTER - ${this.commands}`)
+    }
+
+    public static whatDidUSay(ctx: Context) {
+        return ctx.reply('Desculpe... Não entendi oque você disse :(')
     }
 
     public static getCommands(): iCommand[] {
