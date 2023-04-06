9af52dd162e7796d77dd863733efff1f51b77823
LucaoMendes <lucas1999mendes@gmail.com>
2023-04-06 14:47:48 -0300

    [UserProfiles] Criação da migração, modelo e controller  do objeto

---
+++ src/database/migrations/20230401011639-crete_table_users_profiles.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users_profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        reference: 'users',        
+        unique: true,
+      },
+      min_odd: Sequelize.DOUBLE,
+      max_odd: Sequelize.DOUBLE,
+      team_priority: Sequelize.STRING,
+      max_matches: Sequelize.INTEGER,
+      bet_value: Sequelize.DOUBLE,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users_profiles')
+  }
+};
