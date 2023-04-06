93a6a398394136df3a7b3fddf034a3b41dfb7b2c
LucaoMendes <lucas1999mendes@gmail.com>
2023-04-06 19:20:11 -0300

    [Routines] Rotinas de carregamento de partidas

---
+++ src/routines/LiveMatches.ts
@@ -0,0 +1,26 @@
+import MatchController from "../controllers/MatchController"
+import { iRoutine } from "../interfaces/iRoutine"
+import { FetchCenter } from "../services/FetchCenter"
+import Logger, { LogType } from "../utils/Logger"
+
+async function liveMatches(){
+    
+    const liveMatches = await FetchCenter.getLiveMatches() 
+
+    Logger.send(`${liveMatches.length} partidas ao vivo`,LogType.CRON)
+
+    if(liveMatches.length == 0)
+        return
+
+    await MatchController.saveAll(liveMatches)
+}
+
+const LiveMatches:iRoutine = {
+    name: 'LiveMatches',
+    description: 'Rotina responsável por atualizar os dados das partidas a cada minuto',
+    expression: '* * * * *',
+    active: true,
+    function: liveMatches
+}
+
+export default LiveMatches
---
+++ src/routines/LoadMatches.ts
@@ -0,0 +1,37 @@
+import { iRoutine } from "../interfaces/iRoutine"
+import Logger, { LogType } from "../utils/Logger"
+import { FetchCenter } from "../services/FetchCenter"
+import MatchController from "../controllers/MatchController"
+
+async function loadMatches() {
+    Logger.send('Carregando jogos',LogType.CRON)
+    const today = new Date()
+    const tomorrow = new Date(today)
+    
+    tomorrow.setDate(tomorrow.getDate() + 1)
+
+    const todayString = today.toISOString().split('T')[0]
+    const tomorrowString = tomorrow.toISOString().split('T')[0]
+
+    const todayMatches = await FetchCenter.getDateMatches(todayString)
+    const tomorrowMatches = await FetchCenter.getDateMatches(tomorrowString)
+
+    Logger.send(`Carregados ${todayMatches.length} jogos para hoje e ${tomorrowMatches.length} jogos para amanhã`,LogType.CRON)
+    
+    Logger.send(`Analisando ${todayMatches.length} partidas de hoje`,LogType.CRON)
+    await MatchController.saveAll(todayMatches)
+
+    Logger.send(`Analisando ${tomorrowMatches.length} partidas de amanhã`,LogType.CRON)
+    await MatchController.saveAll(tomorrowMatches)
+}
+
+const LoadMatches:iRoutine = {
+    name: 'LoadMatches',
+    description: 'Realiza o carregamento das partidas do dia',
+    expression: '0 * * * *',
+    active: false,
+    function: loadMatches
+}
+
+
+export default LoadMatches
