f34143d7846c7e15a6f31c1767174c740fe5c914
LucaoMendes <lucas1999mendes@gmail.com>
2023-04-09 22:03:39 -0300

    [LiveMatch] Verificação se todas as partidas já foram finalizadas, se sim ele não puxa as partidas ao vivo

--- src/routines/LiveMatches.ts
+++ src/routines/LiveMatches.ts
@@ -1,10 +1,28 @@
+import { Op } from "sequelize"
 import MatchController from "../controllers/MatchController"
 import { iRoutine } from "../interfaces/iRoutine"
+import Multiple from "../models/Multiple"
 import { FetchCenter } from "../services/FetchCenter"
 import Logger, { LogType } from "../utils/Logger"
+import { eMultipleStatus } from "../utils/MultipleUtils"
 
 async function liveMatches(){
     
+    const anyMatchToCheck = (await Multiple.findAll({
+        where:{
+            status: {
+                [Op.notIn]: [
+                    eMultipleStatus.GREEN,
+                    eMultipleStatus.RED,
+                    eMultipleStatus.CANCELED,
+                ]
+            }
+        }
+    })).length > 0
+
+    if(!anyMatchToCheck)
+        return
+
     const liveMatches = await FetchCenter.getLiveMatches() 
 
     Logger.send(`${liveMatches.length} partidas ao vivo`,LogType.CRON)
